4f56d8f370ad449b78ed3236f2ab1893ff40c5ae
Lenovo-PC <e999or@bk.ru>
2020-03-18 23:01:49 +0300

    mvn test, ide test ok

--- src/test/java/com/github/e999or/lesson20/SimpleTest.java
+++ src/test/java/com/github/e999or/lesson20/SimpleTest.java
@@ -14,10 +14,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-
 public class SimpleTest {
     private static final Logger LOG = LoggerFactory.getLogger(SimpleTest.class);
     private WebDriver webDriver;
